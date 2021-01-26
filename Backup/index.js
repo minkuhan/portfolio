605dfc27ac5ba9ac3bc2de1757480cfe482f24c4
minkuhan <minkuhan15@gmail.com>
2021-01-25 21:30:22 -0500

    New

---
+++ Backup/index.js
@@ -0,0 +1,11 @@
+// const navSlide = () => {
+//     const hamburger = document.querySelector('.hambuger');
+//     const nav = document.querySelector('.nav-links');
+
+//     hamburger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active')
+//     });
+
+// }
+
+// navSlide();
